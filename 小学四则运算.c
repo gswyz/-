e19421ca84e0d9d106249226732957363224e32b
gswyz <42953020+gswyz@users.noreply.github.com>
2018-10-29 14:09:01 +0800

    Add files via upload

--- 小学四则运算.c
+++ 小学四则运算.c
@@ -9,6 +9,9 @@ float algorithm2(float x,char op2,float y);
 char createnumberop();
 void test1(int n);
 
+
+
+
 main()
 {
 	int chooce,n;
@@ -16,12 +19,18 @@ main()
 	{
 		printf("\t请输入你要选择的习题：\n");
 		printf("\n\t1.100以内四则运算 (小数点后保留两位)\n\n");
+		printf("\n\t2.整数四则运算\n\n");
+		printf("\n\t3.改变背景颜色\n\n");
 		do{
 			printf("选择为：");
 			scanf("%d",&chooce);
-			if(chooce!=1)
+			if(chooce!=1 && chooce!=2 && chooce!=3)
 				printf("【输入有误，请重新输入!】\n");
-		}while(chooce!=1);
+		}while(chooce!=1 && chooce!=2 && chooce!=3);
+		if(chooce==3)
+        {
+            blank();
+        }
 		printf("\n请输入四则运算的数目：");
 		while(1)
             {
@@ -35,6 +44,10 @@ main()
 		{
 			test1(n);
 		}
+		if(chooce==2)
+        {
+            test2(n);
+        }
 	}
 }
 
@@ -121,10 +134,70 @@ void test1(int n)
 				}
 		}
 	}
-	printf("你总共答对了%d题，准确率为%.2f％!\n",n,rightnum,(float)rightnum/(float)n*100);
+	printf("你总共答对了%d题，准确率为%.2f％!\n",rightnum,(float)rightnum/(float)n*100);
 	system("pause");
 	system("CLS");
 	printf("\n\n");
+}
+
+void test2(int n){
+    int a, answer, result,i,b,rightnum=0,wrongnum=0;
+    srand(time(NULL));
+    for(i=0;i<n;i++){
+    a=rand() % 10+1;//获取随机数
+    printf("%d!=",a);
+    result=jiecheng(a);
+    scanf("%d",&answer);
+    if(answer==result)
+    {
+        printf("回答正确!\n\n");
+        rightnum++;
+    }
+    else{
+        printf("回答错误!\n\n");
+        wrongnum++;
+    }
+    }
+    printf("你总共答对了%d题，准确率为%.2f％!\n",rightnum,(float)rightnum/(float)n*100);
+	system("pause");
+	system("CLS");
+	printf("\n\n");
+}
+
+int jiecheng(int a){
+    int f;
+    if(a==0 || a==1)
+    {
+        f=1;
+    }
+    else{
+        f=jiecheng(a-1)*a;
+    }
+    return f;
+}
+
+void blank(){
+    int a;
+    printf("请选择背景颜色：");
+    printf("1 = 黑色 ，2 = 灰色，3 = 蓝色， 4 = 淡蓝色，5 = 绿色 ，6 = 淡绿色，7 = 湖蓝色 \n 8 = 淡浅绿色，9 = 红色， 10 = 淡红色，11 = 紫色 ，12 = 淡紫色，13 = 黄色 ，14 = 淡黄色\n");
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:system("color 0f");break;
+        case 2:system("color 8f");break;
+        case 3:system("color 1f");break;
+        case 4:system("color 9f");break;
+        case 5:system("color 2f");break;
+        case 6:system("color Af");break;
+        case 7:system("color 3f");break;
+        case 8:system("color Bf");break;
+        case 9:system("color 4f");break;
+        case 10:system("color cf");break;
+        case 11:system("color 5f");break;
+        case 12:system("color Df");break;
+        case 13:system("color 6f");break;
+        case 14:system("color Ef");break;
+    }
 }
 
 
